e1dd8d4cd6ff2ef755c6803dc816adaba3e55e2c
syl4 <syl4@illinois.edu>
2016-11-16 02:16:56 -0600

    very little list infrastructure

--- app/list-view/_list-view-module.js
+++ app/list-view/_list-view-module.js
@@ -6,5 +6,20 @@ angular.module('app.course-list-view', [])
     bindings: {
 
     },
+    controller: ListViewController,
     templateUrl: 'list-view/list.html'
   });
+
+ListViewController.$inject = ['CourseRegistrationService', '$scope'];
+function ListViewController(CourseRegistrationService, $scope) {
+  var vm = this;
+  vm.courses = CourseRegistrationService.courses;
+  // TODO: filter courses into registered and preview
+  // vm.registeredCourses
+  // vm.previewCourses
+
+  // Watch? I think if new courses are added/removed, vm.courses wouldn't change -- not binded
+  $scope.$watch('CourseRegistrationService.courses', function(oldVal, newVal) {
+    vm.courses = newVal;
+  });
+}
