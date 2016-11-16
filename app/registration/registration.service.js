754c80f9f3fe475b4c92ef5e360cd936848f6d36
Lloyd Ramey <lnr0626@gmail.com>
2016-11-15 22:19:34 -0500

    Add super simple registration service

---
+++ app/registration/registration.service.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('app.registration')
+.factory('CourseRegistrationService', CourseRegistrationService);
+
+CourseRegistrationService.$inject = [];
+function CourseRegistrationService() {
+  var courses = {};
+
+  function addCourse(section) {
+    courses[section.id] = section;
+  }
+
+  function removeCourse(section) {
+    delete courses[section.id];
+  }
+
+  return {
+    courses: courses,
+    addCourse: addCourse,
+    addCourses: function(courses) {
+      courses.forEach(addCourse);
+    },
+    removeCourse: removeCourse,
+    removeCourses: function(courses) {
+      courses.forEach(removeCourse);
+    }
+  };
+}
